c261fddb93a46fa0ef57348e35660315aad634be
Julian Romera Juarez <julian.romera@gmail.com>
2012-08-20 07:20:54 +0300

    Update app/models/movie.rb

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,10 +1,9 @@
 class Movie < ActiveRecord::Base
-<<<<<<< HEAD
+
   @@ratings = ['G', 'PG', 'PG-13', 'R']
 
   def self.get_ratings
     ratings
   end
-=======
->>>>>>> 2ff1e2cd6859061d47d89d4d504512de3734d3d3
+
 end
